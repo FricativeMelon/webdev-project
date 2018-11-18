c98d220c832a9cf42584411d5ff27c8b599945f0
hurtadoisabel <isa.hurcuesta@gmail.com>
2018-11-18 14:59:20 -0500

    foreign keys in database and style in login page

--- priv/repo/migrations/20181117031005_create_friends.exs
+++ priv/repo/migrations/20181117031005_create_friends.exs
@@ -3,11 +3,12 @@ defmodule Project.Repo.Migrations.CreateFriends do
 
   def change do
     create table(:friends) do
-      add :user_id, :integer
+      add :user_id, references(:users, on_delete: :delete_all), null: false
       add :friend_id, :integer
 
       timestamps()
     end
 
+      create index(:friends, [:user_id])
   end
 end
--- priv/repo/migrations/20181117031136_create_calendars.exs
+++ priv/repo/migrations/20181117031136_create_calendars.exs
@@ -4,9 +4,10 @@ defmodule Project.Repo.Migrations.CreateCalendars do
   def change do
     create table(:calendars) do
       add :timestamp, :date
+      add :user_id, references(:users, on_delete: :delete_all), null: false
 
       timestamps()
     end
-
+    create index(:calendars, [:user_id])
   end
 end
