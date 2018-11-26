86b105b8ec7255fef402e6212dc0119303e65b2b
hurtadoisabel <isa.hurcuesta@gmail.com>
2018-11-26 11:51:37 -0500

    password verifications

--- lib/project/users/user.ex
+++ lib/project/users/user.ex
@@ -6,7 +6,11 @@ defmodule Project.Users.User do
   schema "users" do
     field :email, :string
     field :name, :string
+
     field :password_hash, :string
+
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
 
     timestamps()
   end
@@ -14,7 +18,38 @@ defmodule Project.Users.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:email, :name, :password_hash])
+    |> cast(attrs, [:email, :name, :password, :password_confirmation])
+    |> validate_confirmation(:password)
+    |> validate_password(:password)
+    |> put_pass_hash()
     |> validate_required([:email, :name, :password_hash])
+  
   end
+
+  def validate_password(changeset, field, options \\ []) do
+    validate_change(changeset, field, fn _, password ->
+      case validate_password?(password) do
+        {:ok, _} -> []
+        {:error, msg} -> [{field, options[:message] || msg}]
+      end
+    end)
+  end
+
+  def put_pass_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+      change(changeset, Comeonin.Argon2.add_hash(password))
+  end
+  
+  def put_pass_hash(changeset), do: changeset
+
+  def validate_password?(password) when byte_size(password) > 3 do
+    {:ok, password}
+  end
+
+  def validate_password?(_), do: {:error, "The password is too short"}
+
+
+  
+
+
+  
 end
