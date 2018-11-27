67f58bca04bc2566152883c91cc28e081c7a1d58
hurtadoisabel <isa.hurcuesta@gmail.com>
2018-11-26 20:25:40 -0500

    goals working

--- priv/repo/migrations/20181117025949_create_goals.exs
+++ priv/repo/migrations/20181117025949_create_goals.exs
@@ -3,8 +3,8 @@ defmodule Project.Repo.Migrations.CreateGoals do
 
   def change do
     create table(:goals) do
-      add :user_id, references(:users, on_delete: :delete_all), null: false
-      add :description, :text
+      add :user_id, references(:users, on_delete: :delete_all)
+      add :description, :string
 
       timestamps()
     end
